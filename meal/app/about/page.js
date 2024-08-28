ddc5ced33ed15a85c95ded5476d7b1b69ae2c326
Renolation <vodanh.2901@gmail.com>
2024-08-29 00:53:46 +0700

    ss

---
+++ meal/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+    return (
+        <main>
+            <h1>About this course</h1>
+            <p>This course is designed to help you get started with NextJS. We'll cover the basics of routing, data fetching, and more!</p>
+        </main>
+    );
+}
