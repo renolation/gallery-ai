975f19645812f13d29249ce3ba633ad92d0cfe08
Renolation <vodanh.2901@gmail.com>
2024-09-04 00:28:10 +0700

    auth

--- auth/actions/auth-action.js
+++ auth/actions/auth-action.js
@@ -3,6 +3,7 @@
 import {createUser} from "@/lib/user";
 import {hashUserPassword} from "@/lib/hash";
 import {redirect} from "next/navigation";
+import {createAuthSession} from "@/lib/auth";
 
 export async function signup(prevState, formData) {
     const email = formData.get('email');
@@ -25,9 +26,12 @@ export async function signup(prevState, formData) {
     }
     const hashedPassword = hashUserPassword(password);
     try {
-        createUser(email, hashedPassword);
+        const id = createUser(email, hashedPassword);
+        await createAuthSession(id)
+
+        redirect('/training');
     } catch (error) {
-        if(error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+        if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
             return {
                 errors: {
                     email: 'email already exists'
@@ -36,6 +40,6 @@ export async function signup(prevState, formData) {
         }
         throw error;
     }
-    redirect('/training');
+
 
 }
